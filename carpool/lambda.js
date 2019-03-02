abb0321a08f601e3629af702da023746c31789eb
danclemson <danclemson@gmail.com>
2019-03-02 13:46:23 -0500

    update

--- carpool/lambda.js
+++ carpool/lambda.js
@@ -1,11 +1,34 @@
 let AWS = require('aws-sdk');
+const sns = new AWS.SNS();
 
-exports.handler = function(event, context, callback) {
+exports.handler = function (event, context, callback) {
     let receiver = event['receiver'];
     let sender = event['sender'];
     let message = event['messaage'];
 
     console.log("Sending message", message, "to receiver", receiver);
-    
-    callback(null, {"message": "Successfully executed"});
+    sns.publish({
+        Message: message,
+        MessageAttributes: {
+            'AWS.SNS.SMS.SMSType': {
+                DataType: 'String',
+                StringValue: 'Promotional'
+            },
+            'AWS.SNS.SMS.SenderID': {
+                DataType: 'String',
+                StringValue: sender
+            },
+        },
+        PhoneNumber: receiver
+    }).promise()
+        .then(data => {
+            console.log("Send message to", receiver);
+            callback(null, data);
+        })
+        .catch(err => {
+            console.log("Sending failed", err);
+            callback(err);
+        });
+
+    callback(null, { "message": "Successfully executed" });
 }
