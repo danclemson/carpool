6ea1c75fd5114d799e68bc12e0643c1cbb6b49fb
danclemson <danclemson@gmail.com>
2019-03-02 13:37:31 -0500

    update

--- carpool/lambda.js
+++ carpool/lambda.js
@@ -1,4 +1,11 @@
+let AWS = require('aws-sdk');
+
 exports.handler = function(event, context, callback) {
+    let receiver = event['receiver'];
+    let sender = event['sender'];
+    let message = event['messaage'];
+
+    console.log("Sending message", message, "to receiver", receiver);
     
     callback(null, {"message": "Successfully executed"});
 }
